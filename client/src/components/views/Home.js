7c3d22109163e8605e464e3a336245810bc44f7d
unknown <yw73@rice.edu>
2019-05-18 14:53:21 -0500

    updates to homepage html

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -9,8 +9,26 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div>
-                
+            <div class = 'background'>
+                <h1>Donations Hook Up</h1>
+                <div class = 'searchBox'>
+                    <div class = 'box'>
+                        <h2>I want to donate.</h2>
+                        <form class='donate'>
+                            Zip Code: <input type = 'number' class = 'zipCode' name ='donate_zip_code'></input>
+                            Within Miles:  <input type = 'number' class = 'miles' name = 'donate_miles'></input>                       
+                            <input type='submit' name = 'donate_submit' value = 'Search'></input>
+                        </form>
+                    </div>
+                    <div class='box'>
+                        <h2>Seek donated goods.</h2>
+                        <form class='request'>
+                            Zip Code: <input type='number' class='zipCode' name = 'request_zip_code'></input>
+                            Within Miles:  <input type='number' class='request_miles'></input>
+                            <input type='submit' name='request_submit' value='Search'></input>
+                        </form>
+                    </div>
+                </div>
             </div>  
         );
     }
